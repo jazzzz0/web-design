257495e20db049844aa402682debb049083e3cba
jorgelinaju <149181087+jorgelinaju@users.noreply.github.com>
2024-08-30 21:10:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function validarRespuesta(estado, respuesta, pregunta) {
     let feedback = "";
     var botonRespuestaSeleccionada = document.getElementById("respuesta" + respuesta);
 
-     // Limpiar las clases anteriores de todos los botones en la pregunta actual
-     var botones = document.querySelectorAll("#pregunta" + pregunta + " .btn");
-     botones.forEach(boton => boton.classList.remove("correcto", "incorrecto"));
+    // Limpiar las clases anteriores de todos los botones en la pregunta actual
+    var botones = document.querySelectorAll("#pregunta" + pregunta + " .btn");
+    botones.forEach(boton => boton.classList.remove("correcto", "incorrecto"));
 
-     // Añadir la clase correspondiente al botón seleccionado
+    // Añadir la clase correspondiente al botón seleccionado
     if (estado === 1) {
         botonRespuestaSeleccionada.classList.add("correcto");
         feedback = "Correcto";
@@ -22,9 +22,13 @@ function validarRespuesta(estado, respuesta, pregunta) {
 function muestraPreguntaSiguiente(pregunta) {
     setTimeout(() => {
         var preguntaSiguiente = pregunta + 1;
-    
+
         var header = document.getElementById("header");
         
+        // Cambiar el número de la pregunta en el header
+        document.getElementById("numeroPregunta").textContent = "Pregunta número: " + preguntaSiguiente;
+
+        // Cambiar la categoría y el color del header
         if (preguntaSiguiente === 2) {
             document.getElementById("tituloCategoria").textContent = "Categoría: Geografía";
             header.classList.remove("historia", "arte");
@@ -36,12 +40,12 @@ function muestraPreguntaSiguiente(pregunta) {
         } else if (preguntaSiguiente === 4) {
             document.getElementById("numeroPregunta").style.display = "none";
             document.getElementById("tituloCategoria").textContent = "Fin";
-        } else {
-            document.getElementById("numeroPregunta").textContent = "Pregunta número: " + preguntaSiguiente;
         }
 
+        // Ocultar la pregunta actual y mostrar la siguiente
         document.getElementById("feedback").textContent = "";
         document.getElementById("pregunta" + pregunta).style.display = "none";
         document.getElementById("pregunta" + preguntaSiguiente).style.display = "block";
     }, 5000);
 }
+
